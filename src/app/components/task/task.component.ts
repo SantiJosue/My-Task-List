60f8364eebbfc57acfd7b6f8a15a8e54a7877085
santicomercio7@gmail.com <santicomercio7@gmail.com>
2022-11-10 18:52:02 -0300

    componente de lista de items

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Task} from "../../Task";
 import {TASKS} from "../../mock-tasks";
 
+
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
