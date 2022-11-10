8d6fdbdd7b45775b45f3cca100018cb920e92e14
santicomercio7@gmail.com <santicomercio7@gmail.com>
2022-11-10 18:01:32 -0300

    Agregando mock de tareas y el interface de las mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "Terminar perimer modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras",
+        day: "Agosto 5 a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Bootstrap",
+        day: "Agosto 5 a las 19:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Lerr mi libro favorito",
+        day: "Agosto 5 a las 21:00",
+        reminder: false
+    }
+]
